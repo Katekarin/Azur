78a2e432fc87980430456b92e52bd618c2416f43
Katekarin <94514885+Katekarin@users.noreply.github.com>
2023-05-11 12:19:09 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import abort, redirect, url_for, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')    
+def about():
+    return app.send_static_file('about.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
